95c693f878ff2003e29248bbffcdfb1447c1bfba
hafidzpro <mbakgopie@gmail.com>
2023-12-16 13:53:37 +0700

    add menu: highlight & underline

--- projekk.py
+++ projekk.py
@@ -1,8 +1,40 @@
 import sys
 import mysql.connector
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QAction, QMenu, QFontDialog, QShortcut
-from PyQt5.QtGui import QKeySequence, QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QAction, QMenu, QFontDialog, QShortcut, QMessageBox, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QDialogButtonBox
+from PyQt5.QtGui import QKeySequence, QFont, QImage, QTextDocumentFragment
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QTextCharFormat, QColor
+
+class ImageResizeDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Resize Image")
+        self.setGeometry(200, 200, 300, 150)
+
+        layout = QVBoxLayout()
+
+        self.width_label = QLabel("Width:")
+        self.width_edit = QLineEdit()
+        layout.addWidget(self.width_label)
+        layout.addWidget(self.width_edit)
+
+        self.height_label = QLabel("Height:")
+        self.height_edit = QLineEdit()
+        layout.addWidget(self.height_label)
+        layout.addWidget(self.height_edit)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        layout.addWidget(button_box)
+
+        self.setLayout(layout)
+
+    def get_size(self):
+        width = int(self.width_edit.text()) if self.width_edit.text().isdigit() else 0
+        height = int(self.height_edit.text()) if self.height_edit.text().isdigit() else 0
+        return width, height
 
 class App(QMainWindow):
     def __init__(self):
@@ -29,8 +61,8 @@ class App(QMainWindow):
         open_action.triggered.connect(self.open_file)
         file_menu.addAction(open_action)
 
-        self.open_recent_menu = QMenu("Open Recent", self)  # Moved this line from update_open_recent_menu
-        file_menu.addMenu(self.open_recent_menu)  # Moved this line from update_open_recent_menu
+        self.open_recent_menu = QMenu("Open Recent", self)
+        file_menu.addMenu(self.open_recent_menu)
 
         save_action = QAction("Save", self)
         save_action.triggered.connect(self.save_file)
@@ -43,6 +75,7 @@ class App(QMainWindow):
         file_menu.addAction(exit_action)
 
         edit_menu = menubar.addMenu("Edit")
+
         cut_action = QAction("Cut", self)
         cut_action.triggered.connect(self.cut)
         edit_menu.addAction(cut_action)
@@ -55,6 +88,19 @@ class App(QMainWindow):
         paste_action.triggered.connect(self.paste)
         edit_menu.addAction(paste_action)
 
+        highlight_action = QAction("Highlight", self)
+        highlight_action.triggered.connect(self.highlight_text)
+        edit_menu.addAction(highlight_action)
+
+        underline_action = QAction("Underline", self)
+        underline_action.triggered.connect(self.underline_text)
+        edit_menu.addAction(underline_action)
+
+
+        insert_image_action = QAction("Insert Image", self)
+        insert_image_action.triggered.connect(self.insert_image)
+        edit_menu.addAction(insert_image_action)
+
         font_menu = menubar.addMenu("Font")
         font_type_action = QAction("Font Type", self)
         font_type_action.triggered.connect(self.choose_font)
@@ -66,6 +112,21 @@ class App(QMainWindow):
 
         shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_A), self)
         shortcut.activated.connect(self.select_all)
+
+
+    def highlight_text(self):
+     cursor = self.text.textCursor()
+     char_format = QTextCharFormat()
+     char_format.setBackground(QColor(Qt.yellow))
+     cursor.setCharFormat(char_format)
+
+    def underline_text(self):
+     cursor = self.text.textCursor()
+     char_format = QTextCharFormat()
+     char_format.setFontUnderline(True)
+     cursor.setCharFormat(char_format)
+
+
 
     def connectd(self):
         self.connection = mysql.connector.connect(
@@ -137,7 +198,32 @@ class App(QMainWindow):
         self.text.copy()
 
     def paste(self):
-        self.text.paste()
+        clipboard = QApplication.clipboard()
+        mime_data = clipboard.mimeData()
+
+        if mime_data.hasText():
+            text_fragment = QTextDocumentFragment.fromHtml(mime_data.text())
+            cursor = self.text.textCursor()
+            cursor.insertFragment(text_fragment)
+
+    def insert_image(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Image File", "", "Images (*.png *.jpg *.bmp *.gif);;All Files (*)", options=options)
+
+        if file_name:
+            try:
+                dialog = ImageResizeDialog(self)
+                if dialog.exec_() == QDialog.Accepted:
+                    width, height = dialog.get_size()
+                    image = QImage(file_name)
+                    if width > 0 and height > 0:
+                        image = image.scaled(width, height, Qt.KeepAspectRatio)
+                    cursor = self.text.textCursor()
+                    cursor.insertImage(image)
+            except Exception as e:
+                QMessageBox.critical(self, "Error", f"An error occurred while inserting the image:\n{str(e)}")
 
     def select_all(self):
         self.text.selectAll()
